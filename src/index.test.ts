75778aabc366fb803c8493bb0bb83b5c629ba729
uri <uri.valevski@gmail.com>
2024-01-12 01:11:59 +0200

    nicer-api

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,10 @@
-import { equals, pipe, sideEffect } from "gamla";
-import { SimplifiedNode, findInSimplifiedTree, simplifyHtml } from "./index.ts";
+import { equals, pipe, sideEffect, take } from "gamla";
+import {
+  SimplifiedNode,
+  findInSimplifiedTree,
+  mainList,
+  simplifyHtml,
+} from "./index.ts";
 import { assertEquals } from "assert";
 
 const writeToFile = <T>(obj: T) =>
@@ -21,7 +26,7 @@ Deno.test("test runs without errors", () => {
   ["imdb1.html", "moviequotes.html"].forEach(simplifyFile);
 });
 
-Deno.test("test title link with colon after", () => {
+Deno.test("colon", () => {
   const desiredString =
     "Cal Jacobs : I'm envious of your generation, you know. You guys don't care as much about the rules.";
   assertEquals(
@@ -30,4 +35,13 @@ Deno.test("test title link with colon after", () => {
     ),
     desiredString,
   );
+});
+
+Deno.test("duckduck", () => {
+  assertEquals(mainList(simplifyFile("duckduckgo.html"))?.[0], {
+    title: "Snow White and the Seven Dwarfs (1937) - IMDb",
+    url: "www.imdb.com/title/tt0029583/fullcredits",
+    paragraph:
+      "Directed by Writing Credits Cast verified as complete Produced by Walt Disney ... producer (uncredited) Music by Art Direction by Second Unit Director or Assistant Director Sound Department Visual Effects by Camera and Electrical Department Animation Department Costume and Wardrobe Department Editorial Department Music Department Additional Crew",
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,162 @@
+import { complement, empty, filter, map, max, sideLog } from "gamla";
 import {
-  always,
   cond,
-  empty,
   equals,
-  filter,
   findInTree,
-  head,
-  identity,
   length,
-  map,
   pipe,
   reduce,
+  reduceTree,
   replace,
-  trimWhitesapce,
-} from "https://deno.land/x/gamla@39.0.0/src/index.ts";
+  trimWhitespace,
+} from "https://deno.land/x/gamla@41.0.0/src/index.ts";
 
 import he from "npm:html-entities";
-import { HTMLElement, NodeType, TextNode, parse } from "npm:node-html-parser";
+import { HTMLElement, Node, NodeType, parse } from "npm:node-html-parser";
 
-export type SimplifiedNode = null | string | SimplifiedNode[];
-
-export const findInSimplifiedTree = (
-  predicate: (node: SimplifiedNode) => boolean,
-) =>
-  findInTree(predicate, (x: SimplifiedNode): SimplifiedNode[] =>
-    x instanceof Array ? x : [],
-  );
+type AnnotatedText = Record<string, string>;
 
 const clean = pipe(he.decode, replace(/\s+/g, " "));
+
 const concatStrings = pipe(
   reduce(
     (s: string, x: SimplifiedNode) => clean(s) + " " + clean(x as string),
     () => "",
   ),
-  trimWhitesapce,
+  trimWhitespace,
 );
 
-const handlers: [
-  (children: SimplifiedNode[]) => boolean,
+const isString = (x: SimplifiedNode): x is string => typeof x === "string";
+
+const isElement = (node: Node): node is HTMLElement =>
+  node.nodeType === NodeType.ELEMENT_NODE;
+
+const isTextNode = (node: Node) => node.nodeType === NodeType.TEXT_NODE;
+
+const isBadNode = (node: Node) =>
+  isElement(node) && ["SCRIPT", "STYLE", "NOSCRIPT"].includes(node.tagName);
+
+const detectTitle = (node: Node) =>
+  isElement(node) && /h\d/i.test(node.tagName);
+
+const current = (current: Node, _: SimplifiedNode[]) => current;
+
+const children = (_: Node, children: SimplifiedNode[]) => children;
+
+const unwrap = ([element]: SimplifiedNode[]) => element;
+
+type PredicateAndHandler = [
+  (current: Node, children: SimplifiedNode[]) => boolean,
+  (current: Node, children: SimplifiedNode[]) => SimplifiedNode,
+];
+
+const isUrl = (x: string) =>
+  /^\w.*/.test(x) && /(www)|(https?)|(\.com)|\.org/.test(x) && /[^\s]+/.test(x);
+
+const textBasedAnnotation = (t: string): AnnotatedText | string =>
+  isUrl(t) ? { url: t } : t;
+
+const childrenHandlers: [
+  (children: SimplifiedNode[]) => null | boolean | string,
   (children: SimplifiedNode[]) => SimplifiedNode,
 ][] = [
   [
-    (titleAndContent: SimplifiedNode[]) =>
-      titleAndContent?.length === 2 &&
-      titleAndContent?.every((x) => typeof x === "string") &&
-      (titleAndContent?.[1] as string).startsWith(":"),
+    (childs: SimplifiedNode[]) =>
+      childs.length === 2 &&
+      childs.every(isString) &&
+      (childs[0].endsWith(":") || childs[1].startsWith(":")),
     concatStrings,
   ],
   [
-    (arr: SimplifiedNode[]) =>
-      arr.every((x) => typeof x === "string") && arr.length > 2,
-    concatStrings,
+    (childs: SimplifiedNode[]) => childs.every(isString) && childs.length > 2,
+    pipe(concatStrings, textBasedAnnotation),
   ],
-  [pipe(length, equals(1)), head],
-  [empty<SimplifiedNode>, always(null)],
+  [pipe(length<SimplifiedNode>, equals(1)), unwrap],
+  [empty<SimplifiedNode>, () => null],
   [
-    (x: SimplifiedNode) => typeof x === "string",
-    (x: SimplifiedNode) => (x as string).trim(),
+    ([x, y, z]: SimplifiedNode[]) =>
+      // @ts-expect-error too complex to infer
+      [x, y].every(complement(isString)) && x?.title && y?.url && isString(z),
+    // @ts-expect-error too complex to infer
+    ([x, y, z]: SimplifiedNode[]) => ({ ...x, ...y, paragraph: z }),
   ],
-  [() => true, identity],
+  [
+    () => true,
+    (x) => {
+      // console.log(`unrecognized ${JSON.stringify(x, null, 2)}`);
+      return x;
+    },
+  ],
 ];
 
-const isBadNode = (node: HTMLElement) =>
-  ["SCRIPT", "STYLE", "NOSCRIPT"].includes(node.tagName);
-
-const convertToTreeNode = (node: TextNode | HTMLElement): SimplifiedNode =>
-  node.nodeType === NodeType.TEXT_NODE
-    ? node.innerText.trim() && node.innerText.trim() !== "&nbsp;"
-      ? clean(node.innerText.trim())
-      : null
-    : isBadNode(node as HTMLElement)
-    ? null
-    : pipe(
-        map(convertToTreeNode),
+const handlers: PredicateAndHandler[] = [
+  [pipe(current, isBadNode), () => null],
+  [
+    pipe(current, isTextNode),
+    pipe(current, ({ innerText }: Node) =>
+      innerText.trim() && innerText.trim() !== "&nbsp;"
+        ? textBasedAnnotation(clean(innerText.trim()))
+        : null,
+    ),
+  ],
+  [
+    pipe(current, detectTitle),
+    pipe(
+      children,
+      filter((x: SimplifiedNode) => x),
+      (children: SimplifiedNode[]) => ({
+        title: children.every(isString)
+          ? concatStrings(children)
+          : (children[0] as string),
+      }),
+    ),
+  ],
+  // @ts-expect-error too complex inference
+  ...childrenHandlers.map(
+    map((f) =>
+      pipe(
+        children,
         filter((x: SimplifiedNode) => x),
-        cond(handlers),
-      )(node.childNodes as (TextNode | HTMLElement)[]);
+        f,
+      ),
+    ),
+  ),
+];
 
-export const simplifyHtml = pipe((x) => parse(x, {}), convertToTreeNode);
+const getChildren = (node: Node) => (isElement(node) ? node.childNodes : []);
+
+export const simplifyHtml = pipe(
+  (x) => parse(x, {}),
+  reduceTree(getChildren, cond(handlers)),
+);
+
+export type SimplifiedNode = AnnotatedText | null | string | SimplifiedNode[];
+
+export const findInSimplifiedTree = (
+  predicate: (node: SimplifiedNode) => boolean,
+) =>
+  findInTree(predicate, (x: SimplifiedNode): SimplifiedNode[] =>
+    Array.isArray(x)
+      ? x
+      : isString(x)
+      ? []
+      : x === null
+      ? []
+      : Object.values(x),
+  );
+
+export const mainList = reduceTree(
+  (node: SimplifiedNode) => (Array.isArray(node) ? node : []),
+  (
+    current: SimplifiedNode,
+    children: (null | SimplifiedNode[])[],
+  ): null | SimplifiedNode[] => {
+    const childrenCandidates = children.filter(Array.isArray);
+    const candidates: SimplifiedNode[][] = Array.isArray(current)
+      ? [current, ...childrenCandidates]
+      : childrenCandidates;
+    return empty(candidates)
+      ? null
+      : max(length<SimplifiedNode>)(sideLog(candidates));
+  },
+);
